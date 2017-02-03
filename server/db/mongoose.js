d6646e6ff47579c2aac0b997037425d96ef6e731
kephin <wunshon76325@gmail.com>
2017-02-03 22:34:19 +0800

    set up seperate test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // mongoose maintain collections over time, so we don't have to use callback function as MongoClient did
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose,
