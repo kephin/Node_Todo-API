b49f446d8c290898f8571f60e03b0e89b3056e90
kephin <wunshon76325@gmail.com>
2017-02-11 23:03:03 +0800

    fix typo and arragne

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,36 +1,39 @@
+const data = { id: 'kevin' };
+
+//===========
+//  SHA256
+//===========
 const { SHA256 } = require('crypto-js');
 
-const message = 'I am user #1'
+const message = 'I am user #1';
 const hash = SHA256(message).toString();
 
-const data = { id: 'kevin', };
-const token = {
+const token_SHA256 = {
   data,
-  hash: SHA256(JSON.stringify(data) + 'secretInfo').toString();
-}
+  hash: SHA256(JSON.stringify(data) + 'secretInfo').toString(),
+};
 
-const resultHash = SHA256(JSON.stringify(token.data) + 'secretInfo').toString();
-if (resultHash === token.hash) {
+const resultHash = SHA256(JSON.stringify(token_SHA256.data) + 'secretInfo').toString();
+if (resultHash === token_SHA256.hash) {
   console.log('Data was not changed');
 } else {
   console.log('Data was changed. Don\'t trust it');
 }
 
-//=================================================
-
+//================
+//  JsonWebToken
+//================
 const jwt = require('jsonwebtoken');
 
-const data = {
-  id = 'kevin',
-}
-const token = jwt.sign(data, 'secretInfo');
-console.log(token);
+const token_jwt = jwt.sign(data, 'secretInfo');
+console.log(token_jwt);
 
-const decode = jwt.verify(token, 'secretInfo');
+const decode = jwt.verify(token_jwt, 'secretInfo');
 console.log(decode);
 
-//=================================================
-
+//============
+//  bcrypt
+//============
 const bcrypt = require('bcryptjs');
 
 const password = '123abc';
