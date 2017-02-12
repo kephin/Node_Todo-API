88ea56da7b59bd294b1e89f732853762d9889563
kephin <wunshon76325@gmail.com>
2017-02-12 23:19:07 +0800

    test DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -326,3 +326,23 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', testUsers[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        User
+          .findById(testUsers[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+});
